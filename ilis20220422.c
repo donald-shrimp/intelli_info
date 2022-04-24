1b3c275f11afb4e4ba9fe642e4c94499ef7b7838
donald-shrimp <t0706244@gmail.com>
2022-04-24 22:27:55 +0900

    なんか適当に書いてる

--- ilis20220422.c
+++ ilis20220422.c
@@ -4,32 +4,22 @@
 #define N 256 // 1行の最大文字数(バイト数)
 #define ROW 150 // 読み込むファイルのデータの行数
 #define SIZE_OF_ARRAY(array)    (sizeof(array)/sizeof(array[0]))
- 
-// 1列目の項目名と2列目以降のデータを格納する配列をメンバにもつ構造体
+#define K 1  // kの数
+
 // １．パラメータの設定：データ数，クラス数，特徴空間の次元数，k-最近傍法の k の値など 
+// 1列目の項目名と2列目以降のデータを格納する配列をメンバにもつ構造体
 typedef struct iris {
 	float iris_data[4];
     int iris_label;
 } data;
  
+typedef struct match {
+	float distance[4];
+    int label;
+} match;
+
 // ２．Iris.txt のデータの入力
-/*
-    文字列を分割する
 
-    注意
-        s の文字列内に、区切り文字が連続する箇所があるとき、その隙間は無視される。
-        たとえば、s が "a//b"、separator が "/" のとき、分割結果は、
-        "a" と "b" であり、戻り値は 2 を返す。
-
-    引数
-        s: 対象の文字列。書き換えが起こる。NULL不可
-        separator: 区切り文字の並び。NULL不可
-        result: 結果を格納する文字列配列。十分な要素数を確保すること
-        result_size: 引数result の要素数
-
-    戻り値
-        分割後の文字列の個数
-*/
 int isDelimiter(char p, char delim){
   return p == delim;
 }
@@ -62,9 +52,7 @@ int main(void) {
 	FILE *fp; // FILE型構造体
 	char fname[] = "iris.txt";
 	char line[N];
-    int label,unchi = 0;
 	char* tmp[32];
- 
 	data data[ROW];
  
 	fp = fopen(fname, "r"); // ファイルを開く。失敗するとNULLを返す。
@@ -73,29 +61,40 @@ int main(void) {
 		return -1;
 	}
  
+	int unchi=0;
 	while(fgets(line, N, fp) != NULL) {
 		int count;
 		count = split(tmp, line, ',');
 		for(int i = 0; i < count; i++){
-			printf("%s\n" ,tmp[i]);
+			// printf("%s\n" ,tmp[i]);
 			if(i==4){
 				data[unchi].iris_label=atof(tmp);
 			}else{
 				data[unchi].iris_data[i]=atof(tmp);
 			}
 		}
-		printf("now on roop poop:%d\n",&unchi);
-		unchi+=1;
+		unchi=unchi+1;
 	}
 	fclose(fp); // ファイルを閉じる
 
-// printf("%f,%f,%f,%f,%d\n",&data[1].iris_data[1], &data[1].iris_data[1], &data[1].iris_data[1], &data[1].iris_data[1], &data[1].iris_label);
-// float ymax=5.0,ymin=1.0,xmax=8.0,xmin=4.0;
-
-}
 
 // ３．Iris データの２つの特徴量の取りうる値の範囲を事前に求める 
+	float xmax=5.0,xmin=1.0,ymax=8.0,ymin=4.0;//計算めんどいのでこれでいいや
 
 // ４．３で求めた範囲内で，未知データの 2 成分をキーボード入力する 
+	char coordinate[20];
+	float x,y;
+	do{
+		printf("x,yのように座標を入力してね xは%f~%fの値,yは%f~%fまでの値が入力できるよ！\n",xmin,xmax,ymin,ymax);
+		scanf("%s",coordinate);
+		sscanf(coordinate,"%f,%f",&x,&y);
+		printf("x=%f,y=%f\n",x,y);
+	}while(x<xmin||x>xmax||y<ymin||y>ymax);
 
 // ５．k-最近傍法のアルゴリズムで未知データのクラス番号（0,1,2 のいずれか）を決定して，出力する 
+	// int class_counter[3]=0;
+	match result[K];
+	for(i=0;i<150;i++){
+		l = (x1-x2)*(x1-x2) + (y1-y2)*(y1-y2);
+	}
+}
